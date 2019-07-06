257949b1a6e5cf8d2058177d9487e9b381a6624e
Ryan Kinder <kinderrs@gmail.com>
2019-07-06 08:40:07 -0400

    initial commit

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    players = relationship("Player", back_populates="owner")
+
+
+class Player(Base):
+    __tablename__ = "players"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    handicap = Column(Integer, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="players")
+
---
+++ app/schemas.py
@@ -0,0 +1,40 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class PlayerBase(BaseModel):
+    name: str
+    handicap: int = 0
+
+
+class PlayerCreate(PlayerBase):
+    pass
+
+
+class Player(PlayerBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[Player] = []
+
+    class Config:
+        orm_mode = True
+
+class UserInDB(User):
+    hashed_password: str
