a6517bc0c9a61a81f2f11cd8bb326278be51c3bc
Ryan Kinder <kinderrs@gmail.com>
2019-07-07 10:00:22 -0400

    Added todo item to comments

--- app/crud.py
+++ app/crud.py
@@ -16,7 +16,8 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
+    # TODO: actually generate a hashed password and store it
+    fake_hashed_password = user.password   
     db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
     db.add(db_user)
     db.commit()
