d3832042a97d170a6a2c7635e6da82a36106b61e
Ryan Kinder <kinderrs@gmail.com>
2019-07-07 19:28:51 -0400

    Added support for JWT and hashed passwords

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from typing import List
+from datetime import datetime, timedelta
+
+import jwt
+from jwt import PyJWTError
 
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -15,13 +18,15 @@ from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from starlette.status import HTTP_401_UNAUTHORIZED
 
+from typing import List
+
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
 
 # to get a string like this run:
 # openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = "0e9f5655234f3f56a7633c7d159097f83ca9b90dc129c21015372b8100a7ba22"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -50,31 +55,64 @@ async def db_session_middleware(request: Request, call_next):
     return response
 
 ##########################################################################
-# Dependencies
+# Dependency Functions
 ##########################################################################
 def get_db(request: Request):
     return request.state.db
 
 
+def verify_password_hash(password: str, hashed_password: str):
+    return pwd_context.verify(password, hashed_password)
+
+
 def hash_password(password: str):
-    return password
+    return pwd_context.hash(password)
+
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = crud.get_user_by_email(db, username)
+    if not user:
+        return False
+    if not verify_password_hash(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(*, data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 # decodes the token, gets the user object to pass back
-def decode_token(username: str, db: Session):
+def get_user(db: Session, username: str):
     user = crud.get_user_by_email(db, email=username)
     return user
 
 
 # provides a check on the user if it's authenticated
-async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    user = decode_token(token, db)
-    if not user:
-        raise HTTPException(
-            status_code=400,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
+async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme), ):
+    credentials_exception = HTTPException(
+        status_code=HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate":"Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(email=email)
+    except PyJWTError:
+        raise credentials_exception
+    user = get_user(db, token_data.email)
+    if user is None:
+        raise credentials_exception
     return user
 
 
@@ -83,6 +121,13 @@ async def get_current_active_user(current_user: schemas.User = Depends(get_curre
     if current_user.is_active == 0:
        raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
+
+
+##########################################################################
+# User Functions
+##########################################################################
+
+
 
 
 ##############################################################################
@@ -128,15 +173,21 @@ async def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_
     return items
 
 
-@app.post("/token")
-async def login(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
-    user = crud.get_user_by_email(db, form_data.username)
+@app.post("/token", response_model=schemas.Token)
+async def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
-        raise HTTPException(status_code=400, detail="Incorrect username or password (username)")
-    hashed_password = hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password (password)")
-    return { "access_token": user.email, "token_type": "bearer" }
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED, 
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate":"Bearer"}
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, 
+        expires_delta=access_token_expires
+    )
+    return { "access_token": access_token, "token_type": "bearer" }
 
 
 @app.get("/debug/me", response_model=schemas.User)
