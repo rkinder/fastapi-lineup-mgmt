dab67e03f94759dddd6c37eec1ee72715b2dafe3
Ryan Kinder <kinderrs@gmail.com>
2019-07-07 12:36:22 -0400

    Initial commit

---
+++ app/auth.py
@@ -0,0 +1,50 @@
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+from sqlalchemy.orm import Session
+
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
+
+def get_db(request: Request):
+    return request.state.db
+
+
+def hash_password(password: str):
+    return password
+
+
+# decodes the token, gets the user object to pass back
+def decode_token(token: str, db: Session = Depends(get_db)):
+    user = crud.get_user(db, token)
+    return user
+
+
+# provides a check on the user if it's authenticated
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+# provides a check on whether the account is active, returns user
+async def get_current_active_user(current_user: schemas.User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
