8b0da4ba7586c2d566e9fc2094f09a145fc15086
Ryan Kinder <kinderrs@gmail.com>
2019-07-07 16:34:38 -0400

    non-functional collection of auth functions

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
@@ -7,6 +5,8 @@ from sqlalchemy.orm import Session
 
 from starlette.requests import Request
 from starlette.responses import Response
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 from starlette.status import HTTP_401_UNAUTHORIZED
 
 from . import crud, models, schemas
@@ -25,26 +25,25 @@ def hash_password(password: str):
 
 
 # decodes the token, gets the user object to pass back
-def decode_token(token: str, db: Session = Depends(get_db)):
-    user = crud.get_user(db, token)
+def decode_token(username: str, db: Session):
+    user = crud.get_user_by_email(db, email=username)
     return user
 
-
 # provides a check on the user if it's authenticated
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = decode_token(token)
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    user = decode_token(token,db)
     if not user:
         raise HTTPException(
-            status_code=HTTP_401_UNAUTHORIZED,
+            status_code=400,
             detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
+            headers={"WWW-Authenticate": "Bearer"}
         )
     return user
 
 
 # provides a check on whether the account is active, returns user
 async def get_current_active_user(current_user: schemas.User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
+    if current_user.is_active:
+       raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
